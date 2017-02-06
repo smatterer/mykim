dcc9e31e6044f3038705d1ca0f2ed344b914a22c
whp <81807220@qq.com>
2017-02-07 07:07:20 +0800

    yii2框架初始化 版本 2.0.11 添加web公共接口和api应用

---
+++ web/admin.php
@@ -0,0 +1,27 @@
+<?php
+//开发环境
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+//生产环境
+//defined('YII_ENV') or define('YII_ENV', 'prod');
+
+
+//require(__DIR__ . '/../common/helper/function.php');//自定义函数文件
+
+// 定义网站根目录
+//define('ROOT_PATH',dirname(__FILE__));
+//define('DS', DIRECTORY_SEPARATOR);// 定义WIN和Linux操作系统分隔符
+
+require(__DIR__ . '/../vendor/autoload.php');
+require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+require(__DIR__ . '/../common/config/bootstrap.php');
+require(__DIR__ . '/../backend/config/bootstrap.php');
+
+$config = yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . '/../common/config/main.php'),
+    require(__DIR__ . '/../common/config/main-local.php'),
+    require(__DIR__ . '/../backend/config/main.php'),
+    require(__DIR__ . '/../backend/config/main-local.php')
+);
+
+(new yii\web\Application($config))->run();
---
+++ web/api.php
@@ -0,0 +1,30 @@
+<?php
+//开发环境
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+//生产环境
+//defined('YII_ENV') or define('YII_ENV', 'prod');
+
+
+//require(__DIR__ . '/../common/helper/function.php');//自定义函数文件
+
+// 定义网站根目录
+//define('ROOT_PATH',dirname(__FILE__));
+//define('DS', DIRECTORY_SEPARATOR);// 定义WIN和Linux操作系统分隔符
+
+
+
+require(__DIR__ . '/../vendor/autoload.php');
+require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+require(__DIR__ . '/../common/config/bootstrap.php');
+require(__DIR__ . '/../api/config/bootstrap.php');
+
+$config = yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . '/../common/config/main.php'),
+    require(__DIR__ . '/../common/config/main-local.php'),
+    require(__DIR__ . '/../api/config/main.php'),
+    require(__DIR__ . '/../api/config/main-local.php')
+);
+
+
+(new yii\web\Application($config))->run();
